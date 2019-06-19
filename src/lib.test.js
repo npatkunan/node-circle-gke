bcb8a1b77d4390bb95005f5d5091496bdac15ab4
Kharyb <30594862+Kharyb@users.noreply.github.com>
2019-06-19 18:35:55 +0100

    update message [ci skip]

--- src/lib.test.js
+++ src/lib.test.js
@@ -1,5 +1,5 @@
 const lib = require('./lib');
 
 test('helloWorld() returns some special text', () => {
-  expect(lib.helloWorld()).toBe('Hello World I am deployed in Kubernetes!');
+  expect(lib.helloWorld()).toBe('Hello World! I have been deployed in Kubernetes!');
 });
